1b85d5496e569007246de5e22ed0beed91d32217
Javi <jmarin0802@gmail.com>
2020-05-20 18:18:14 -0400

    Creacion de commands

---
+++ src/main/java/guru/javi/recipeprojectguru/commands/Ingredient.java
@@ -0,0 +1,44 @@
+package guru.javi.recipeprojectguru.commands;
+
+import java.math.BigDecimal;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(exclude = {"recipe"})
+@Entity
+public class Ingredient {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String description;
+	private BigDecimal amount;
+	
+	@ManyToOne
+	private Recipe recipe;
+	
+	@OneToOne(fetch = FetchType.EAGER)
+	private UnitOfMeasure unitOfMeasure;
+
+	public Ingredient(String description, BigDecimal amount, UnitOfMeasure unitOfMeasure) {
+		this.description = description;
+		this.amount = amount;
+		this.unitOfMeasure = unitOfMeasure;
+	}
+	public Ingredient() {
+		
+	}
+}
---
+++ src/main/java/guru/javi/recipeprojectguru/commands/Recipe.java
@@ -0,0 +1,82 @@
+package guru.javi.recipeprojectguru.commands;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.Lob;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+
+import guru.javi.recipeprojectguru.enump.Difficulty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@Entity
+public class Recipe {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String description;
+	private Integer prepTime;
+	private Integer cookTime;
+	private Integer servings;
+	private String source;
+	private String url;
+	
+	@Lob
+	private String directions;
+	
+	@Lob
+	private Byte[] image;
+	
+	@Enumerated(EnumType.STRING)
+	private Difficulty difficulty; 
+	
+	@OneToOne(cascade = CascadeType.ALL)
+	private Notes notes;
+	
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
+	private Set<Ingredient> ingredients = new HashSet<>();
+	
+	@ManyToMany
+	@JoinTable(name = "recipe_category", 
+	joinColumns = @JoinColumn(name = "recipe_id"), 
+	inverseJoinColumns = @JoinColumn(name = "category_id"))
+	private Set<Category> categories = new HashSet<>();
+	
+
+	public void setNotes(Notes notes) {
+		this.notes = notes;
+		notes.setRecipe(this);
+	}
+
+	public Recipe addIngredients(Ingredient ingredient) {
+		ingredients.add(ingredient);
+		ingredient.setRecipe(this);
+		return this;
+	}
+
+	public Recipe(String description, Integer prepTime, Integer cookTime, Difficulty difficulty, String directions) {
+		super();
+		this.description = description;
+		this.prepTime = prepTime;
+		this.cookTime = cookTime;
+		this.directions = directions;
+		this.difficulty = difficulty;
+	}
+	
+	public Recipe() {}
+}
